2a696478d341d7a3e2e5f903110b84d5875388af
mdsaifurrahman117 <saifurrahmansaif954@gmail.com>
2025-05-13 00:24:23 +0600

    navbar added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router';
 import Footer from './../components/Footer';
+import Navbar from '../components/Navbar';
 
 const Root = () => {
           return (
                     <>
                               <Header />
+                              <Navbar />
                               <Outlet />
                               <Footer />
                     </>
